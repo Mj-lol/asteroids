bf2df5d66d3375c6c760d216e1bb19c86661d99b
Mj-lol <michaelpics52@gmail.com>
2024-10-10 17:57:05 -0500

    get shot

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from asteroid import *
 from asteroidfield import *
 from constants import *
 from player import *
-
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,9 +16,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     dt = 0
     player=Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     f = AsteroidField()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+from shot import Shot
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
@@ -27,6 +28,11 @@ class Player(CircleShape):
             self.move(-1*dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    def shoot(self):
+        s = Shot(self.position.x, self.position.y)
+        s.velocity = pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
